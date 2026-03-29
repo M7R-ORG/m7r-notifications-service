c38ab0f822883fe363e3cdea5d6b1b95c747300e
SiAvMaAnR <siavmaanr@gmail.com>
2026-03-30 00:25:19 +0300

    fix: clean up email module, use NODE_OPTIONS for DNS instead

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -5,7 +5,6 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
-import { lookup } from 'dns';
 
 @Module({
   controllers: [EmailController],
@@ -29,7 +28,6 @@ async function mailerModuleFactory(config: ConfigService) {
         user: config.get<string>('email.user'),
         pass: config.get<string>('email.password'),
       },
-      dnsOptions: { lookup },
     },
     defaults: {
       from: `"No Reply" <${config.get<string>('email.from')}>`,
