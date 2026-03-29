1069b4a09a546dd71a861199dc9a6e7c3d661691
SiAvMaAnR <siavmaanr@gmail.com>
2026-03-29 23:46:33 +0300

    fix: use dns.lookup for SMTP to resolve IPv6 correctly

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -5,6 +5,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
+import { lookup } from 'dns';
 
 @Module({
   controllers: [EmailController],
@@ -28,6 +29,7 @@ async function mailerModuleFactory(config: ConfigService) {
         user: config.get<string>('email.user'),
         pass: config.get<string>('email.password'),
       },
+      dnsOptions: { lookup },
     },
     defaults: {
       from: `"No Reply" <${config.get<string>('email.from')}>`,
